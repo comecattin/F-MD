2277e0006f73d65038eb9956b0e9cbbb2ea5c770
Côme Cattin <come.cattin@ens-lyon.fr>
2024-05-27 15:25:12 +0200

    Set the x and y limits as dependent of the positions.

--- src/plot_trajectories.py
+++ src/plot_trajectories.py
@@ -25,21 +25,27 @@ def read_trajectories(filename):
     return steps, positions
 
 def animate(i):
+
     ax.clear()
     pos = positions[i]
     ax.scatter(pos[:, 0], pos[:, 1], s=10)
-    ax.set_xlim(-1, 11)
-    ax.set_ylim(-1, 11)
+    ax.set_xlim(0, x_max)
+    ax.set_ylim(0, y_max)
     ax.set_title(f'Time Step: {steps[i]}')
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
 
 def main():
-    global steps, positions, ax
+    global steps, positions, ax, x_max, y_max
     steps, positions = read_trajectories('trajectories.dat')
 
     steps = steps[::20]
     positions = positions[::20]
+
+    # x and y limits depend on the positions
+    positions = np.array(positions)
+    x_max = np.max(positions[:, :, 0])
+    y_max = np.max(positions[:, :, 1])
 
     fig, ax = plt.subplots(figsize=(8, 8))
     ani = FuncAnimation(fig, animate, frames=len(steps), repeat=False)
