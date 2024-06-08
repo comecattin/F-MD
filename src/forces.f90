4f28c26b80c99a79583eaf5f7786d449d1194792
Côme Cattin <come.cattin@ens-lyon.fr>
2024-06-08 13:42:41 +0200

    Compute forces first implementation

--- src/forces.f90
+++ src/forces.f90
@@ -45,23 +45,77 @@ contains
         integer, intent(in) :: n
         real(8), intent(in) :: box_length
         integer :: i, j
-        real(8), dimension(3) :: dist
-        real(8) :: r, r2, coulomb_force
+        real(8), dimension(3) :: dist, vec1, vec2
+        real(8) :: r, r2, f_yukawa, force_lj, force_harmonic_bond, force_harmonic_angle
+        integer :: bond_type
+        real(8) :: cos_theta, theta
 
-        call compute_forces(position, forces, n, box_length)
+        real(8) :: alpha, g
+        real(8) :: ka, kb, r_eq, angle_eq
 
-        ! Add Coulomb forces
+        logical :: is_OO, is_OH, is_HH
+
+        alpha = 1.0
+        g = 1.0
+        r_eq = 1.012                     ! Angstrom
+        angle_eq = 113.24                ! Deg
+        angle_eq = angle_eq * pi / 180.0 ! Rad
+        ka = 75.90
+        kb = 1059.162
+
+        forces = 0.0
+
         do i = 1, n-1
+
+            ! Harmonic potential on HOH angle
+            if (mod(i, 3) == 1) then
+                vec1 = position(i+1, :) - position(i, :)
+                vec2 = position(i+2, :) - position(i, :)
+                
+                cos_theta = dot_product(vec1, vec2) / (norm2(vec1) * norm2(vec2))
+                theta = acos(cos_theta)
+                
+                call harmonic(theta, angle_eq, ka, force_harmonic_angle)
+
+                forces(i, :) = forces(i, :) - force_harmonic_angle * (vec1/norm2(vec1) + vec2/norm2(vec2))
+                forces(i + 1, :) = forces(i + 1, :) - force_harmonic_angle * vec1/norm2(vec1)
+                forces(i + 2, :) = forces(i + 2, :) - force_harmonic_angle * vec2/norm2(vec2)
+            end if
+
             do j = i+1, n
+
+                bond_type = mod(i, 3) + mod(j, 3)
+                bond_type = mod(bond_type, 3)
+
+                is_OH = (bond_type == 1 .or. bond_type == 0)
+                is_OO = (bond_type == 2 .and. mod(i, 3) == 1)
+                is_HH = (bond_type == 2 .and. mod(i, 3) /= 1)
                 
                 dist = position(i, :) - position(j, :)
                 r2 = sum(dist**2)
                 r = sqrt(r2)
 
-                coulomb_force = charges(i) * charges(j) / r2
+                ! Oxygen Oxygen interaction
+                if (is_OO) then
+                    ! Lennard Jones forces
+                    call lennard_jones(r, force_lj)
+                    force_lj = 0.0
+                    forces(i, :) = forces(i, :) + force_lj * dist/r
+                    forces(j, :) = forces(j, :) - force_lj * dist/r
 
-                forces(i, :) = forces(i, :) + coulomb_force * dist / r
-                forces(j, :) = forces(j, :) - coulomb_force * dist / r  
+                ! Oxygen Hydrogen interaction
+                else if (is_OH) then
+                    ! Harmonic bond forces
+                    call harmonic(r, r_eq, kb, force_harmonic_bond)
+                    forces(i, :) = forces(i, :) + force_harmonic_bond * dist/r
+                    forces(j, :) = forces(j, :) - force_harmonic_bond * dist/r
+                
+                end if
+
+                ! Coulomb force for all pair
+                call coulomb_yukawa(r, 1.0_8, charges(i), charges(j), alpha, g, f_yukawa)
+                forces(i, :) = forces(i, :) + f_yukawa * dist/r
+                forces(j, :) = forces(j, :) - f_yukawa * dist/r
 
             end do
         end do
