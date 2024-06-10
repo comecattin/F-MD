4aac2395a1ab9bfa669b67a991ff71bf1ddf58ae
Côme Cattin <come.cattin@ens-lyon.fr>
2024-06-10 14:40:15 +0200

    Yukawa potential

--- src/energies.f90
+++ src/energies.f90
@@ -1,7 +1,13 @@
 module energies_module
     implicit none
-    real(8), parameter :: epsilon = 1.0 ! LJ potential well depth
-    real(8), parameter :: sigma = 1.0 ! LJ distance where potential is zero
+    real(8), parameter :: pi = 3.14159265358979323846
+    real(8), parameter :: epsilon = 0.1554253          ! kcal/mol
+    real(8), parameter :: sigma = 3.165492             ! Angstrom 
+    real(8), parameter :: ka_HOH = 75.90               ! kcal.mol-1.rad-2
+    real(8), parameter :: kb_OH = 1059.162             ! kcal.mol-1.angstrom-2
+    real(8), parameter :: alpha = 1.0
+    real(8), parameter :: r_eq_OH = 1.012              ! Angstrom
+    real(8), parameter :: angle_eq = 113.24            ! deg
     
 contains 
     subroutine compute_energies(pos, vel, charges, n, ke, pe, te)
@@ -40,6 +46,9 @@ contains
         real(8) :: r
 
         pe = 0.0
+
+
+        ! Lennard-Jones potential
         do i = 1, n
             do j = i + 1, n
                 r = sqrt(sum((pos(i, :) - pos(j, :)) ** 2))
@@ -47,25 +56,16 @@ contains
             end do
         end do
     end subroutine compute_potential_energy
-    
-    subroutine compute_potential_energy_water(pos, n, charges, pe)
-        real(8), dimension(n, 3), intent(in) :: pos
-        integer, intent(in) :: n
-        real(8), dimension(n), intent(in) :: charges
-        real(8), intent(out) :: pe
-        integer :: i, j
-        real(8) :: r
 
-        pe = 0.0
-        do i = 1, n
-            do j = i + 1, n
-                r = sqrt(sum((pos(i, :) - pos(j, :)) ** 2))
-                ! Lennard Jones potential
-                pe = pe + 4.0 * epsilon * ((sigma / r) ** 12 - (sigma / r) ** 6)
-                ! Coulomb potential
-                pe = pe + charges(i) * charges(j) / r
-            end do
-        end do
-    end subroutine compute_potential_energy_water
+    subroutine yukawa_potential(r, mass, q1, q2, alpha_param, yukawa_p)
+        
+        real(8), intent(in) :: r
+        real(8), intent(in) :: q1, q2
+        real(8), intent(in) :: mass, alpha_param
+        real(8), intent(out) :: yukawa_p
+
+        yukawa_p = - q1*q2 * exp(-alpha_param * mass * r) / r
+
+    end subroutine yukawa_potential
     
 end module energies_module
