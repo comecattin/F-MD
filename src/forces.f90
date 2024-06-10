3b02303ac1ee8d202e2e413369c01874180c632c
Côme Cattin <come.cattin@ens-lyon.fr>
2024-06-10 14:23:50 +0200

    Compute forces in 3 different loops

--- src/forces.f90
+++ src/forces.f90
@@ -1,8 +1,14 @@
 module forces_module
     implicit none
-    real(8), parameter :: epsilon = 0.1554253 ! LJ O-O SPC/Fw
-    real(8), parameter :: sigma = 3.165492    ! LJ O-O SPC/Fw
     real(8), parameter :: pi = 3.14159265358979323846
+    real(8), parameter :: epsilon = 0.1554253          ! kcal/mol
+    real(8), parameter :: sigma = 3.165492             ! Angstrom 
+    real(8), parameter :: ka_HOH = 75.90               ! kcal.mol-1.rad-2
+    real(8), parameter :: kb_OH = 1059.162             ! kcal.mol-1.angstrom-2
+    real(8), parameter :: alpha = 1.0
+    real(8), parameter :: g = 1.0
+    real(8), parameter :: r_eq_OH = 1.012              ! Angstrom
+    real(8), parameter :: angle_eq = 113.24            ! deg
     
 contains
 
@@ -49,74 +55,67 @@ contains
         real(8) :: r, r2, f_yukawa, force_lj, force_harmonic_bond, force_harmonic_angle
         integer :: bond_type
         real(8) :: cos_theta, theta
-
-        real(8) :: alpha, g
-        real(8) :: ka, kb, r_eq, angle_eq
-
+        real(8) :: angle_eq_rad
         logical :: is_OO, is_OH, is_HH
 
-        alpha = 1.0
-        g = 1.0
-        r_eq = 1.012                     ! Angstrom
-        angle_eq = 113.24                ! Deg
-        angle_eq = angle_eq * pi / 180.0 ! Rad
-        ka = 75.90
-        kb = 1059.162
+        
+        angle_eq_rad = angle_eq * pi / 180.0 ! Rad
+        
 
         forces = 0.0
 
-        do i = 1, n-1
-
+        ! Intra molecular interactions
+        do i = 1, n-1, 3
             ! Harmonic potential on HOH angle
-            if (mod(i, 3) == 1) then
-                vec1 = position(i+1, :) - position(i, :)
-                vec2 = position(i+2, :) - position(i, :)
-                
-                cos_theta = dot_product(vec1, vec2) / (norm2(vec1) * norm2(vec2))
-                theta = acos(cos_theta)
-                
-                call harmonic(theta, angle_eq, ka, force_harmonic_angle)
+            vec1 = position(i+1, :) - position(i, :)
+            vec2 = position(i+2, :) - position(i, :)
 
-                forces(i, :) = forces(i, :) - force_harmonic_angle * (vec1/norm2(vec1) + vec2/norm2(vec2))
-                forces(i + 1, :) = forces(i + 1, :) - force_harmonic_angle * vec1/norm2(vec1)
-                forces(i + 2, :) = forces(i + 2, :) - force_harmonic_angle * vec2/norm2(vec2)
-            end if
+            ! Minimum image convention
+            vec1 = vec1 - nint(vec1 / box_length) * box_length
+            vec2 = vec2 - nint(vec2 / box_length) * box_length
+            
+            cos_theta = dot_product(vec1, vec2) / (norm2(vec1) * norm2(vec2))
+            theta = acos(cos_theta)
+            
+            call harmonic(theta, angle_eq_rad, ka_HOH, force_harmonic_angle)
 
-            do j = i+1, n
+            forces(i, :) = forces(i, :) - force_harmonic_angle * (vec1/norm2(vec1) + vec2/norm2(vec2))
+            forces(i + 1, :) = forces(i + 1, :) - force_harmonic_angle * vec1/norm2(vec1)
+            forces(i + 2, :) = forces(i + 2, :) - force_harmonic_angle * vec2/norm2(vec2)
 
-                bond_type = mod(i, 3) + mod(j, 3)
-                bond_type = mod(bond_type, 3)
 
-                is_OH = (bond_type == 1 .or. bond_type == 0)
-                is_OO = (bond_type == 2 .and. mod(i, 3) == 1)
-                is_HH = (bond_type == 2 .and. mod(i, 3) /= 1)
-                
+            ! Harmonic potential on O-H bond
+            !    O-H1 bond
+            call harmonic(norm2(vec1), r_eq_OH, kb_OH, force_harmonic_bond)
+            forces(i, :) = forces(i, :) + force_harmonic_bond * vec1/norm2(vec1)
+            forces(i + 1, :) = forces(i + 1, :) - force_harmonic_bond * vec1/norm2(vec1)
+            
+            !    O-H2 bond
+            call harmonic(norm2(vec2), r_eq_OH, kb_OH, force_harmonic_bond)
+            forces(i, :) = forces(i, :) + force_harmonic_bond * vec2/norm2(vec2)
+            forces(i + 2, :) = forces(i + 2, :) - force_harmonic_bond * vec2/norm2(vec2)
+            
+        end do
+
+        ! Van der Waals interactions
+        do i = 1, n-4, 3
+            do j = i+3, n, 3
                 dist = position(i, :) - position(j, :)
-                r2 = sum(dist**2)
-                r = sqrt(r2)
+                ! Minimum image convention
+                dist = dist - nint(dist / box_length) * box_length
+                r = norm2(dist)
+                call lennard_jones(r, force_lj)
+                forces(i, :) = forces(i, :) + force_lj * dist/r
+                forces(j, :) = forces(j, :) - force_lj * dist/r
+            end do
+        end do
 
-                ! Oxygen Oxygen interaction
-                if (is_OO) then
-                    ! Lennard Jones forces
-                    call lennard_jones(r, force_lj)
-                    force_lj = 0.0
-                    forces(i, :) = forces(i, :) + force_lj * dist/r
-                    forces(j, :) = forces(j, :) - force_lj * dist/r
-
-                ! Oxygen Hydrogen interaction
-                else if (is_OH) then
-                    ! Harmonic bond forces
-                    call harmonic(r, r_eq, kb, force_harmonic_bond)
-                    forces(i, :) = forces(i, :) + force_harmonic_bond * dist/r
-                    forces(j, :) = forces(j, :) - force_harmonic_bond * dist/r
-                
-                end if
-
-                ! Coulomb force for all pair
+        ! Coulomb interactions
+        do i = 1, n-1
+            do j = i+1, n
                 call coulomb_yukawa(r, 1.0_8, charges(i), charges(j), alpha, g, f_yukawa)
                 forces(i, :) = forces(i, :) + f_yukawa * dist/r
                 forces(j, :) = forces(j, :) - f_yukawa * dist/r
-
             end do
         end do
 
@@ -136,12 +135,9 @@ contains
     subroutine lennard_jones(r, force_lj)
     
         real(8), intent(in) :: r
-        real(8) :: r6
         real(8), intent(out) :: force_lj
 
-        r6 = r**6
-        
-        force_lj = 24.0d0 * epsilon * (2.0d0 * sigma**6 / r6**2 - sigma**12 / r6)
+        force_lj = 48*epsilon/sigma * ((sigma / r)**13 - 0.5 * (sigma / r)**7)
 
     end subroutine lennard_jones
 
